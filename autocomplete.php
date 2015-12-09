c1cf13cc0653bcf1f3762b137dff5d9e77cc4569
Batuhan <tuterbatuhan@gmail.com>
2015-12-09 22:59:04 +0200

    Update autocomplete.php

--- autocomplete.php
+++ autocomplete.php
@@ -1,9 +1,9 @@
 
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'batuhan_tuter');
-define('DB_USER','batuhan.tuter'); 
-define('DB_PASSWORD','lcknr6rq8');
+define('DB_NAME', '******');
+define('DB_USER','********'); 
+define('DB_PASSWORD','*******');
 #Connects the DataBase
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
